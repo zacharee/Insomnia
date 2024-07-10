fb77af22ff521f5a5a57816141cd7706e509301c
Zachary Wander <zachary.wander@gmail.com>
2024-07-09 20:09:46 -0400

    Update dependencies and migrate to version catalog

--- app/src/main/java/com/zacharee1/insomnia/compose/ConfigureLayout.kt
+++ app/src/main/java/com/zacharee1/insomnia/compose/ConfigureLayout.kt
@@ -43,7 +43,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.themeadapter.material3.Mdc3Theme
 import com.zacharee1.insomnia.App
@@ -61,7 +60,6 @@ import kotlinx.coroutines.withContext
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
-@Preview
 fun ConfigureLayout(title: String = "TestTitle") {
     val context = LocalContext.current
 
