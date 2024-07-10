0248f531fb2a312660cf3d350731efde9d547746
Zachary Wander <zachary.wander@gmail.com>
2024-07-09 23:19:46 -0400

    Version 1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         namespace = "com.zacharee1.insomnia"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.7"
+        versionCode = 10
+        versionName = "1.8"
     }
     buildTypes {
         release {
